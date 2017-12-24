eafbb550d55101fd05f636c65affbfb6017be77e
yp <yigit.polat@ozu.edu.tr>
2017-12-24 21:09:56 +0300

    with errors

---
+++ Anamnesia/Controller/ThemeController.swift
@@ -0,0 +1,43 @@
+//
+//  ThemeController.swift
+//  Anamnesia
+//
+//  Created by Yigit Polat on 24.12.2017.
+//  Copyright © 2017 Yigit Polat. All rights reserved.
+//
+
+import UIKit
+
+class ThemeController: UIViewController {
+
+    @IBOutlet weak var spaceBox: UIButton!
+    
+    @IBOutlet weak var carBox: UIButton!
+    
+    @IBOutlet weak var girlBox: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
